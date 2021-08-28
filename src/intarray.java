df66842a0df42c475892fcc884dd6d1c90f74294
Saransh Halwai <saransh2304@gmail.com>
2021-08-28 15:54:30 +0530

    Initial commit

---
+++ src/intarray.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class intarray {
+    int[] array;
+    int length;
+    int sum;
+    public intarray(int length)
+    {
+        array=new int[length];
+        this.length=array.length;
+        this.sum();
+    }
+    public intarray(int length,boolean input)
+    {
+        super();
+        array=new int[length];
+        this.length=array.length;
+        if(input)
+            this.input();
+    }
+
+    public void input()
+    {
+        Scanner scan=new Scanner(System.in);
+        for (int i=0;i<length;i++)
+        {
+            array[i]=scan.nextInt();
+        }
+        this.sum();
+    }
+
+    private void sum()
+    {
+        for (int i=0;i<length;i++)
+        {
+            sum+=array[i];
+        }
+    }
+
+    public void print()
+    {
+        for (int i=0;i<length;i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+
+    public void addElement(int element,int index)
+    {
+        array[index]=element;
+    }
+}
